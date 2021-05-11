62eee901b8f93cbca4628805d1b654c9b2951bc6
Cesar <chechaacual@gmail.com>
2021-05-11 11:03:00 -0600

    Crear layout principal

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Products</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('content')
+
 	<h1>List of Products.</h1>
 
 	@empty($products)
@@ -40,5 +36,5 @@
 			</table>
 		</div>
 	@endempty
-</body>
-</html>
+
+@endsection
