e357c3dac6215be64a2611f9f1c5c0afe9639513
Cesar <chechaacual@gmail.com>
2021-06-23 11:08:01 -0600

    Mostrar mensajes de validacion

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,12 +6,22 @@
 	<title>Store</title>
 </head>
 <body>
-	@dump($errors)
 	@if (session()->has('error'))
 		<div class="alert alert-danger">
 			{{ session()->get('error') }}
 		</div>
 	@endif
+
+	@if ($errors->any())
+		<div class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif 
+
 	@yield('content')
 </body>
 </html>
