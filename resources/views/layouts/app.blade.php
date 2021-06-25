6de359ac758d6023e0b75202f414d7e4201c1794
Cesar <chechaacual@gmail.com>
2021-06-25 11:10:35 -0600

    Cambiar de layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,22 @@
         </nav>
 
         <main class="py-4">
+            @if (session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+            @endif
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif 
+            
             @yield('content')
         </main>
     </div>
