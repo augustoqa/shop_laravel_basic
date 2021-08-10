2db0338d637f3ae732ac6142b15427fe9f93ecfa
Cesar Acual <agustoacual@gmail.com>
2021-08-10 16:12:13 -0600

    Relacion entre modelos Order y Payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -10,10 +10,15 @@ class Payment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'amount', 'payed_at',
+        'amount', 'payed_at', 'order_id',
     ];
 
     protected $dates = [
         'payed_at',
     ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
