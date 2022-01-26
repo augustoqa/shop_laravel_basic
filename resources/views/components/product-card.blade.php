ff3d94a6e6d3b09086909fc15a7acf7905741e19
Cesar Acual <agustoacual@gmail.com>
2022-01-26 11:02:07 -0600

    Usando recursos anidados para agregar productos al carrito

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -9,5 +9,13 @@
 		<h5 class="card-title">{{ $product->title }}</h5>
 		<p class="card-text">{{ $product->description }}</p>
 		<p class="card-text"><strong>{{ $product->stock }} left</strong></p>
+		<form 
+			method="POST" 
+			action="{{ route('products.carts.store', ['product' => $product->id]) }}"
+			class="d-inline"
+		>
+			@csrf
+			<button type="submit" class="btn btn-success">Add To Cart</button>
+		</form>
 	</div>
 </div>
