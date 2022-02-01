408ef4b6e475585cae88f2d1445a3cd8f51a5d46
Cesar Acual <agustoacual@gmail.com>
2022-02-01 13:07:11 -0600

    Permitir remover productos del carrito

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,7 +1,7 @@
 <div class="card">
-	<img 
-		src="{{ asset($product->images->first()->path) }}" 
-		alt="{{ $product->title }}" 
+	<img
+		src="{{ asset($product->images->first()->path) }}"
+		alt="{{ $product->title }}"
 		class="card-img-top" height="500"
 	>
 	<div class="card-body">
@@ -9,13 +9,25 @@
 		<h5 class="card-title">{{ $product->title }}</h5>
 		<p class="card-text">{{ $product->description }}</p>
 		<p class="card-text"><strong>{{ $product->stock }} left</strong></p>
-		<form 
-			method="POST" 
-			action="{{ route('products.carts.store', ['product' => $product->id]) }}"
-			class="d-inline"
-		>
-			@csrf
-			<button type="submit" class="btn btn-success">Add To Cart</button>
-		</form>
-	</div>
+        @if (isset($cart))
+        <form
+            method="POST"
+            action="{{ route('products.carts.destroy', ['cart' => $cart->id, 'product' => $product->id]) }}"
+            class="d-inline"
+        >
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-warning">Remove From Cart</button>
+        </form>
+        @else
+        <form
+            method="POST"
+            action="{{ route('products.carts.store', ['product' => $product->id]) }}"
+            class="d-inline"
+        >
+            @csrf
+            <button type="submit" class="btn btn-success">Add To Cart</button>
+        </form>
+        @endif
+    </div>
 </div>
