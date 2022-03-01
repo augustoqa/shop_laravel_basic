66976843ab1607adb5a9b74fc730823a45a6da37
Cesar Acual <chechaacual@gmail.com>
2022-03-01 14:41:06 -0600

    Usar controlador de recursos anidados para cobrar una orden

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -60,5 +60,7 @@ class OrderController extends Controller
             });
 
         $order->products()->attach($cartProductsWithQuantity->toArray());
+
+        return redirect()->route('orders.payments.create', ['order' => $order]);
     }
 }
