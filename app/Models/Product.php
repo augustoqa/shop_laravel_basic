59a595aa2e5de39b723b0360233c81a256078854
César Acual <augustoacual@gmail.com>
2022-06-02 15:58:16 -0600

    Usando global scopes de Eloquent para usar solo los productos disponibles

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\AvailableScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,6 +17,16 @@ class Product extends Model
     	'stock',
     	'status',
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new AvailableScope);
+    }
 
     public function carts()
     {
